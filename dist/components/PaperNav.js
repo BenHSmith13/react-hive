cee4d428e019df012e7c711b693337a61ec77f43
Brandon Beeks <brandon@beeks.me>
2016-10-28 13:19:57 -0600

    v3.0.7

--- dist/components/PaperNav.js
+++ dist/components/PaperNav.js
@@ -28,16 +28,23 @@ var PaperNav = _react2.default.createClass({
   displayName: 'PaperNav',
 
   propTypes: {
-    base_url: _react2.default.PropTypes.string,
     footer: _react2.default.PropTypes.node,
     logo_description: _react2.default.PropTypes.node,
     logo_label: _react2.default.PropTypes.string,
     logo_url: _react2.default.PropTypes.string,
     onLinkClick: _react2.default.PropTypes.func,
     primary: _react2.default.PropTypes.arrayOf(_react2.default.PropTypes.shape(linkStructure)).isRequired,
-    secondary: _react2.default.PropTypes.arrayOf(_react2.default.PropTypes.shape(linkStructure))
+    secondary: _react2.default.PropTypes.arrayOf(_react2.default.PropTypes.shape(linkStructure)),
+    shouldShowLink: _react2.default.PropTypes.func
   },
 
+  getDefaultProps: function getDefaultProps() {
+    return {
+      shouldShowLink: function shouldShowLink() {
+        return true;
+      }
+    };
+  },
   getInitialState: function getInitialState() {
     return {
       active_top_level_menu: null,
@@ -61,100 +68,112 @@ var PaperNav = _react2.default.createClass({
     return window.location.href.indexOf(link.link_route) > -1;
   },
   _renderTopLevelLink: function _renderTopLevelLink(link, i) {
-    var is_active = this.state.active_top_level_menu === link.title || this._checkIfActive(link);
-    var classes = ['paper-list__item core-nav__app', link.children ? 'hasSubmenu' : '', this.state.active_top_level_menu === link.title ? 'isOpen isActive' : '', is_active ? 'isActive' : ''];
+    if (this.props.shouldShowLink(link)) {
+      var is_active = this.state.active_top_level_menu === link.title || this._checkIfActive(link);
+      var classes = ['paper-list__item core-nav__app', link.children ? 'hasSubmenu' : '', this.state.active_top_level_menu === link.title ? 'isOpen isActive' : '', is_active ? 'isActive' : ''];
 
-    return _react2.default.createElement(
-      'li',
-      { className: 'paper-list__row', key: i },
-      _react2.default.createElement(
-        'a',
-        { className: classes.join(' '), onClick: this._handleLinkClick.bind(null, link, 'active_top_level_menu') },
+      return _react2.default.createElement(
+        'li',
+        { className: 'paper-list__row', key: i },
         _react2.default.createElement(
-          'div',
-          { className: 'paper-tile layout horizontal center' },
+          'a',
+          { className: classes.join(' '), onClick: this._handleLinkClick.bind(null, link, 'active_top_level_menu') },
           _react2.default.createElement(
             'div',
-            { className: 'paper-tile__content' },
+            { className: 'paper-tile layout horizontal center' },
             _react2.default.createElement(
               'div',
-              { className: 'hv-size--24' },
-              _react2.default.createElement(_PaperIcon2.default, { icon: link.icon, style: is_active ? { fill: _Styles2.default.Colors.YELLOW } : null })
+              { className: 'paper-tile__content' },
+              _react2.default.createElement(
+                'div',
+                { className: 'hv-size--24' },
+                _react2.default.createElement(_PaperIcon2.default, { icon: link.icon, style: is_active ? { fill: _Styles2.default.Colors.YELLOW } : null })
+              )
+            ),
+            _react2.default.createElement(
+              'div',
+              { className: 'paper-tile__content' },
+              _react2.default.createElement(
+                'h5',
+                null,
+                link.title
+              )
             )
-          ),
+          )
+        ),
+        link.children ? _react2.default.createElement(
+          'ul',
+          { className: 'paper-list paper-list--nav-tree' },
+          link.children.map(this._renderSecondLevelLink)
+        ) : null
+      );
+    } else {
+      return null;
+    }
+  },
+  _renderSecondLevelLink: function _renderSecondLevelLink(link, i) {
+    if (this.props.shouldShowLink(link)) {
+      var classes = ['paper-list__item', link.children ? 'hasSubmenu' : '', this.state.active_second_level_menu === link.title ? 'isOpen' : '', this._checkIfActive(link) ? 'isActive' : ''];
+
+      return _react2.default.createElement(
+        'li',
+        { className: 'paper-list__row', key: i },
+        _react2.default.createElement(
+          'a',
+          { className: classes.join(' '), onClick: this._handleLinkClick.bind(null, link, 'active_second_level_menu') },
           _react2.default.createElement(
             'div',
-            { className: 'paper-tile__content' },
+            { className: 'paper-tile layout horizontal justified center' },
             _react2.default.createElement(
-              'h5',
-              null,
+              'div',
+              { className: 'paper-tile__content' },
+              link.title
+            ),
+            link.children ? _react2.default.createElement(
+              'div',
+              { className: 'paper-tile__content' },
+              _react2.default.createElement(
+                'i',
+                { className: 'material-icons md-18' },
+                'arrow_drop_down'
+              )
+            ) : null
+          )
+        ),
+        link.children ? _react2.default.createElement(
+          'ul',
+          { className: 'paper-list paper-list--nav-tree' },
+          link.children.map(this._renderThirdLevelLink)
+        ) : null
+      );
+    } else {
+      return null;
+    }
+  },
+  _renderThirdLevelLink: function _renderThirdLevelLink(link, i) {
+    if (this.props.shouldShowLink(link)) {
+      var classes = ['paper-list__item', this._checkIfActive(link) ? 'isActive' : ''];
+
+      return _react2.default.createElement(
+        'li',
+        { className: 'paper-list__row', key: i },
+        _react2.default.createElement(
+          'a',
+          { className: classes.join(' '), onClick: this._handleLinkClick.bind(null, link) },
+          _react2.default.createElement(
+            'div',
+            { className: 'paper-tile layout horizontal justified center' },
+            _react2.default.createElement(
+              'div',
+              { className: 'paper-tile__content' },
               link.title
             )
           )
         )
-      ),
-      link.children ? _react2.default.createElement(
-        'ul',
-        { className: 'paper-list paper-list--nav-tree' },
-        link.children.map(this._renderSecondLevelLink)
-      ) : null
-    );
-  },
-  _renderSecondLevelLink: function _renderSecondLevelLink(link, i) {
-    var classes = ['paper-list__item', link.children ? 'hasSubmenu' : '', this.state.active_second_level_menu === link.title ? 'isOpen' : '', this._checkIfActive(link) ? 'isActive' : ''];
-
-    return _react2.default.createElement(
-      'li',
-      { className: 'paper-list__row', key: i },
-      _react2.default.createElement(
-        'a',
-        { className: classes.join(' '), onClick: this._handleLinkClick.bind(null, link, 'active_second_level_menu') },
-        _react2.default.createElement(
-          'div',
-          { className: 'paper-tile layout horizontal justified center' },
-          _react2.default.createElement(
-            'div',
-            { className: 'paper-tile__content' },
-            link.title
-          ),
-          link.children ? _react2.default.createElement(
-            'div',
-            { className: 'paper-tile__content' },
-            _react2.default.createElement(
-              'i',
-              { className: 'material-icons md-18' },
-              'arrow_drop_down'
-            )
-          ) : null
-        )
-      ),
-      link.children ? _react2.default.createElement(
-        'ul',
-        { className: 'paper-list paper-list--nav-tree' },
-        link.children.map(this._renderThirdLevelLink)
-      ) : null
-    );
-  },
-  _renderThirdLevelLink: function _renderThirdLevelLink(link, i) {
-    var classes = ['paper-list__item', this._checkIfActive(link) ? 'isActive' : ''];
-
-    return _react2.default.createElement(
-      'li',
-      { className: 'paper-list__row', key: i },
-      _react2.default.createElement(
-        'a',
-        { className: classes.join(' '), onClick: this._handleLinkClick.bind(null, link) },
-        _react2.default.createElement(
-          'div',
-          { className: 'paper-tile layout horizontal justified center' },
-          _react2.default.createElement(
-            'div',
-            { className: 'paper-tile__content' },
-            link.title
-          )
-        )
-      )
-    );
+      );
+    } else {
+      return null;
+    }
   },
   render: function render() {
     var _this = this;
